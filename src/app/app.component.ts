855d9387116b65c55beba896907415874d9dba3d
Prince Kumar <prince.kumar@thrymr.net>
2022-03-09 18:09:12 +0530

    Worked for filter with datebounceTime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Address } from './address.modal';
+// import { Address } from './address.modal';
+import { Users } from './user.modal';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formsApp';
+
+
+UsersData:Users = new Users ();
+  data: any;
+ address:Address =new Address()
+
+//  ngOnInit() {
+//   this.UsersData.address?.push(this.address)
+//  }
+
+onSubmit(RegisterForm:NgForm){
+    this.data = RegisterForm.value
+   this.data = this.UsersData
+  console.log(this.data ,"user Data");
 }
+
+
+addNewDiv() {
+this.UsersData.address?.push(new Address())
+
+}
+removeAddress(id:any){
+  this.UsersData.address?.splice(id,1)
+}
+
+
+}
+
+
---
+++ src/app/user.modal.ts
@@ -0,0 +1,21 @@
+import { Address } from "./address.modal";
+
+
+
+export class Users {
+
+  constructor(
+    public name?: string,
+    public email?: string,
+    public password?: string,
+    public mobileNumber?: string,
+    public address?: Address[],
+  ) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.email = email;
+    this.mobileNumber = mobileNumber;
+    this.address=[new Address()];
+  }
+}
